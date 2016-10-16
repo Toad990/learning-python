85aacdebcf9e671f4b301731a2e67df013cf3eb6
Hannah Ballingall <hannah.ballingall@gmail.com>
2016-10-16 20:45:49 +0100

    .

--- GhostGame/__init__.py
+++ GhostGame/__init__.py
@@ -4,19 +4,20 @@ print ('Ghost game')
 feeling_brave = True
 score = 0
 while feeling_brave:
-       ghost_door = randint (1, 3)
-       print('three doors ahead')
-       print(' A ghost behind one')
-print (' which door do you open ?')
-door = input('1 ,2 ,3?')
-door_num = int (door)
-if door_num == ghost_door:
-    print ("GHOST !")
-    feeling_brave = False
-else:
-    print (' no ghost!')
-    print ('you enter the next room')
-    score = score + 1
+    ghost_door = randint (1, 3)
+    print (ghost_door)
+    print('three doors ahead')
+    print(' A ghost behind one')
+    print (' which door do you open ?')
+    door = input('1 ,2 ,3?')
+    door_num = int (door)
+    if door_num == ghost_door:
+        print ("GHOST !")
+        feeling_brave = False
+    else:
+        print (' no ghost!')
+        print ('you enter the next room')
+        score = score + 1
 print ('RUN AWAY !')
 print (' Game over! you scored', score)
 
